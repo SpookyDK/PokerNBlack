82aa118be52ba65347682168093e27d58442934b
esben laptop <esben9541@gmail.com>
2023-04-11 13:46:02 +0200

    Start

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+using System; 
+
+namespace Gambling
+
+{
+
+   internal class Program
+
+   {
+
+      static void Main(string[] args)
+
+      {
+
+        Card[] Cards = new Card[52];
+         Console.WriteLine("Hello, World!");
+
+
+        Cards[0].Drawed = false; Cards[0].Name = "ACE"; Cards[0].Value = 1;Cards[0].Color = 1;
+        Cards[1].Drawed = false; Cards[1].Name = "ACE"; Cards[1].Value = 1;Cards[1].Color = 2;
+        Cards[2].Drawed = false; Cards[2].Name = "ACE"; Cards[2].Value = 1;Cards[2].Color = 3;
+        Cards[3].Drawed = false; Cards[3].Name = "ACE"; Cards[3].Value = 1;Cards[3].Color = 4;
+        Cards[4].Drawed = false; Cards[4].Name = "TWO"; Cards[4].Value = 2;Cards[4].Color = 1;
+        Cards[5].Drawed = false; Cards[5].Name = "TWO"; Cards[5].Value = 2;Cards[5].Color = 2;
+        Cards[6].Drawed = false; Cards[6].Name = "TWO"; Cards[6].Value = 2;Cards[6].Color = 3;
+        Cards[7].Drawed = false; Cards[7].Name = "TWO"; Cards[7].Value = 2;Cards[7].Color = 4;
+        Cards[8].Drawed = false; Cards[8].Name = "THREE"; Cards[8].Value = 3;Cards[7].Color = 1;
+        Cards[8].Drawed = false; Cards[8].Name = "THREE"; Cards[8].Value = 3;Cards[7].Color = 2;
+        Cards[8].Drawed = false; Cards[8].Name = "THREE"; Cards[8].Value = 3;Cards[7].Color = 3;
+        Cards[8].Drawed = false; Cards[8].Name = "THREE"; Cards[8].Value = 3;Cards[7].Color = 4;
+        Cards[8].Drawed = false; Cards[8].Name = "FOUR"; Cards[8].Value = 4;Cards[7].Color = 1;
+        Cards[8].Drawed = false; Cards[8].Name = "FOUR"; Cards[8].Value = 4;Cards[7].Color = 2;
+        Cards[8].Drawed = false; Cards[8].Name = "FOUR"; Cards[8].Value = 4;Cards[7].Color = 3;
+        Cards[8].Drawed = false; Cards[8].Name = "FOUR"; Cards[8].Value = 4;Cards[7].Color = 4;
+        Cards[8].Drawed = false; Cards[8].Name = "FIVE"; Cards[8].Value = 5;Cards[7].Color = 1;
+        Cards[8].Drawed = false; Cards[8].Name = "FIVE"; Cards[8].Value = 5;Cards[7].Color = 2;
+        Cards[8].Drawed = false; Cards[8].Name = "FIVE"; Cards[8].Value = 5;Cards[7].Color = 3;
+        Cards[8].Drawed = false; Cards[8].Name = "FIVE"; Cards[8].Value = 5;Cards[7].Color = 4;
+        Cards[8].Drawed = false; Cards[8].Name = "SIX"; Cards[8].Value = 6;Cards[7].Color = 1;
+        Cards[8].Drawed = false; Cards[8].Name = "SIX"; Cards[8].Value = 6;Cards[7].Color = 2;
+        Cards[8].Drawed = false; Cards[8].Name = "SIX"; Cards[8].Value = 6;Cards[7].Color = 3;
+        Cards[8].Drawed = false; Cards[8].Name = "SIX"; Cards[8].Value = 6;Cards[7].Color = 4;
+        Cards[8].Drawed = false; Cards[8].Name = "SEVEN"; Cards[8].Value = 7;Cards[7].Color = 1;
+        Cards[8].Drawed = false; Cards[8].Name = "SEVEN"; Cards[8].Value = 7;Cards[7].Color = 2;
+        Cards[8].Drawed = false; Cards[8].Name = "SEVEN"; Cards[8].Value = 7;Cards[7].Color = 3;
+        Cards[8].Drawed = false; Cards[8].Name = "SEVEN"; Cards[8].Value = 7;Cards[7].Color = 4;
+        Cards[8].Drawed = false; Cards[8].Name = "EIGHT"; Cards[8].Value = 8;Cards[7].Color = 1;
+        Cards[8].Drawed = false; Cards[8].Name = "EIGHT"; Cards[8].Value = 8;Cards[7].Color = 2;
+        Cards[8].Drawed = false; Cards[8].Name = "EIGHT"; Cards[8].Value = 8;Cards[7].Color = 3;
+        Cards[8].Drawed = false; Cards[8].Name = "EIGHT"; Cards[8].Value = 8;Cards[7].Color = 4;
+        Cards[8].Drawed = false; Cards[8].Name = "EIGHT"; Cards[8].Value = 8;Cards[7].Color = 1;
+        
+      }
+
+   }
+
+}
